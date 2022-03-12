2a5291b89fd89826787051ac9d43ab140b70ba37
Christoph Walcher <Christoph.Walcher@gmail.com>
2022-03-12 18:36:33 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# should-release
+
+A minimalistic command line tool to check if a new release should to be published based on semantic commits.
+
+In case no significant commit (breaking change, feat, fix) are detected, the tool will exit with a non-zero exit code.
+
+## Installation
+
+```shell
+npm i -g should-release
+```
+
+## Usage
+
+```shell
+should-release
+
+```
---
+++ cli.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const conventionalRecommendedBump = require("conventional-recommended-bump");
+
+function whatBump(commits) {
+  let shouldRelease = false;
+  let major = 0;
+  let minor = 0;
+  let patch = 0;
+
+  commits.forEach((commit) => {
+    if (commit.notes.length > 0) {
+      shouldRelease = true;
+      major++;
+    } else if (commit.type === "feat") {
+      shouldRelease = true;
+      minor++;
+    } else if (commit.type === "fix") {
+      shouldRelease = true;
+      patch++;
+    }
+  });
+
+  return {
+    shouldRelease,
+    major,
+    minor,
+    patch,
+  };
+}
+
+conventionalRecommendedBump({ whatBump }, { verbose: true }, function (err, data) {
+  if (err) {
+    console.error(err.toString());
+    process.exit(1);
+  }
+
+  console.log(data);
+
+  if (data.shouldRelease) {
+    process.exit(0);
+  } else {
+    process.exit(2);
+  }
+});
